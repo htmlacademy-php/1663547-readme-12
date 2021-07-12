3e187ba5274b59198e36b1526ca99b5b2ad46020
Leonid <pterodyatel@gmail.com>
2021-07-13 01:18:04 +0300

    Работа со строкой запроса, сделал правки, доделал правки

--- index.php
+++ index.php
@@ -9,16 +9,9 @@ require_once('connection.php');
 $is_auth = rand(0, 1);
 $user_name = 'Леонид';
 $title = 'Популярное';
-
 $type_content_id = filter_input(INPUT_GET,'type-content',FILTER_VALIDATE_INT);
-
-if (!$con){
-$error = mysqli_connect_error();
-$page_content = include_template('error.php',['error'=> $error]);
-} else {
-    $result_type = mysqli_query($con, 'SELECT * FROM type_content');
-    $types = mysqli_fetch_all($result_type, MYSQLI_ASSOC);
-    }
+    if ($result_type = mysqli_query($con, 'SELECT * FROM type_content')){
+        $types = mysqli_fetch_all($result_type, MYSQLI_ASSOC);
         $sql_post = 'SELECT p.id, p.heading, p.content, p.image, p.link, p.author_quote,
             p.number_views, u.avatar_path, u.name, t.class_name
             FROM post p
@@ -26,19 +19,22 @@ $page_content = include_template('error.php',['error'=> $error]);
             ON p.users_id = u.id
             JOIN type_content t
             ON p.type_content_id = t.id';
-
-            if ($type_content_id) {
-                $sql_post .= ' WHERE p.type_content_id ='. $type_content_id;
+                if ($type_content_id) {
+                    $sql_post .= ' WHERE p.type_content_id ='. $type_content_id;
             }
-            $sql_post .= ' ORDER BY number_views DESC';
-            $result_post = mysqli_query($con, $sql_post);
-            $posts = mysqli_fetch_all($result_post, MYSQLI_ASSOC);
-            $page_content = include_template('main.php',
-                [
-                'title' => $title,
-                'posts' => $posts,
-                'types' => $types,
-                'type_content_id' => $type_content_id
-                ]);
+        $sql_post .= ' ORDER BY number_views DESC';
+        $result_post = mysqli_query($con, $sql_post);
+        $posts = mysqli_fetch_all($result_post, MYSQLI_ASSOC);
+        $page_content = include_template('main.php',
+            [
+            'title' => $title,
+            'posts' => $posts,
+            'types' => $types,
+            'type_content_id' => $type_content_id
+            ]);
+    } else {
+        $error = mysqli_error($con);
+        $page_content = include_template('error.php', ['error' => $error]);
+    }
 $layout_content = include_template('layout.php', ['content' => $page_content, 'user_name'=> $user_name]);
 print($layout_content);
